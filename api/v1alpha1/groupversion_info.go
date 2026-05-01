872a8894ebd75256bd8e1eddbf660427e71916d8
Scot Schuchert-Wells <wells.scot@gmail.com>
2026-05-01 16:02:41 -0500

    fix: update API group to compliance.miloapis.com

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the compliance v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=compliance.datumapis.com
+// +groupName=compliance.miloapis.com
 package v1alpha1
 
 import (
@@ -12,7 +12,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "compliance.datumapis.com", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "compliance.miloapis.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
